f83c2aa47f37120f53b8b5b17c230e553438257d
AndrelDias-Dev <andre_guki@hotmail.com>
2026-04-22 20:03:37 -0300

    ex003 adicionado

---
+++ Exercícios-2/ex003/script.js
@@ -0,0 +1,9 @@
+function contar() {
+    var i = document.getElementById('ini')
+    var f = document.getElementById('fim')
+    var p = document.getElementById('pas')
+    var res = document.getElementById('res')
+
+    
+
+}    
