06d961dda60b6b1ecc77dc3516f039653c30c893
AndrelDias-Dev <andre_guki@hotmail.com>
2026-04-23 15:00:20 -0300

    ex003 adicionado

--- Exercícios-2/ex003/script.js
+++ Exercícios-2/ex003/script.js
@@ -4,6 +4,24 @@ function contar() {
     var p = document.getElementById('pas')
     var res = document.getElementById('res')
 
-    
+    if (i.value.length == 0 || f.value.length == 0 || p.value.length == 0) {
+        window.alert('[ERRO] Preencha os campos e tente novamente.') 
+    } else {
+        res.innerHTML = 'Contando:'
+        var i = Number(ini.value) 
+        var f = Number(fim.value)
+        var p = Number(pas.value)
+
+        if (i < f) {
+            for (var c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} `
+        }
+        } else {
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c}`
+            }
+        }
+
+    }
 
 }    
