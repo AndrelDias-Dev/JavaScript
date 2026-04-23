1e672cf99681633e71d5dfecb47123c40874ac39
AndrelDias-Dev <andre_guki@hotmail.com>
2026-04-23 18:07:29 -0300

    ex003 adicionado

--- Exercícios-2/ex003/script.js
+++ Exercícios-2/ex003/script.js
@@ -1,26 +1,28 @@
-function gerar() {
+function contar() {
     var ini = document.getElementById('ini')
     var fim = document.getElementById('fim')
     var pas = document.getElementById('pas')
     var res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
-        window.alert('erro')
+        window.alert('ERRO')
     } else {
-        var i = Number(ini.value)
-        var f = Number(fim.value)
-        var p = Number(pas.value)
+        i = Number(ini.value)
+        f = Number(fim.value)
+        p = Number(pas.value)
+
         res.innerHTML = ''
-        
-        if (i < f) {
-        for (var c = i; c <= f; c += p) {
-            res.innerHTML += ` ${c} \u{1F449}`
+
+        if (i <= f) {
+            for (c = i; c <= f; c += p) {
+            res.innerHTML += `${c}`
         }
         } else {
-            for (var c = 1; c >= f; c -= p) {
-                res.innerHTML -= ` ${c} \u{1F449}`
+            for (c = i; c >= f; c -= p) {
+                res.innerHTML += `${c}`
             }
         }
-        res.innerHTML += ` ${c} \u{1F3F4}`
-    }
+        
+     }   
+
 }
