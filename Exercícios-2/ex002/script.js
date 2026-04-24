1d886eee5fa104008f11b61dd5d970407e0e68ec
AndrelDias-Dev <andre_guki@hotmail.com>
2026-04-24 16:09:08 -0300

    ex002 adicionado

--- Exercícios-2/ex002/script.js
+++ Exercícios-2/ex002/script.js
@@ -4,15 +4,15 @@ function verificar() {
     var fano = document.getElementById('num')
     var res = document.getElementById('res')
 
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('ERRO')
+    if (fano.value == 0 || Number(fano.value) >= ano) {
+        window.alert('ERROR')
     } else {
-        var idade = ano - Number(fano.value)
+        var fsex = document.getElementsByName('sex')
         var genero = ''
-        var fsex = document.getElementsByName('sex') 
+        var idade = ano - Number(fano.value)
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
-
+        
         if (fsex[0].checked) {
             genero = 'Homem'
             if (idade >= 5 && idade <= 10) {
@@ -25,20 +25,21 @@ function verificar() {
                 img.setAttribute('src', 'imagens/idoso-h.png')
             }
         } else if (fsex[1].checked) {
-            genero = 'Mulher'
-            if (idade >= 5 && idade <= 10) {
-                img.setAttribute('src', 'imagens/bebe-m.png')
-            } else if (idade <= 25){
-                img.setAttribute('src', 'imagens/jovem-m.png')
-            } else if (idade <= 50) {
-                img.setAttribute('src', 'imagens/adulto-m.png')
-            } else {
-                img.setAttribute('src', 'imagens/idoso-m.png')
-            }
+                   genero= 'Mulher'
+                   if (idade >= 5 && idade <= 10) {
+                    img.setAttribute('src', 'imagens/bebe-h.png')
+                   } else if (idade <= 25) {
+                    img.setAttribute('src', 'imagens/jovem-h.png')
+                   } else if (idade <= 50) {
+                    img.setAttribute('src', 'imagens/adulto-h.png')
+                   } else {
+                    img.setAttribute('src','imagens/idoso-h.png')
+                   }
         }
-
-        res.innerHTML = `Detectamos ${idade} anos e ${genero}.`
-        res.style.textAlign = 'center'
-        res.appendChild(img)
+ 
+    res.innerHTML = `Detectamos ${idade} anos e  ${genero}` 
+    res.style.textAlign = 'center'
+    res.appendChild(img)
     }
+    
 }
