0247f018d21827a94c871256c5c53156c845bd34
AndrelDias-Dev <andre_guki@hotmail.com>
2026-04-25 15:44:28 -0300

    ex002 adicionado

--- Exercícios-2/ex002/script.js
+++ Exercícios-2/ex002/script.js
@@ -1,18 +1,18 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById('num')
+    var fano = document.getElementById('ano')
     var res = document.getElementById('res')
 
-    if (fano.value == 0 || Number(fano.value) >= ano) {
-        window.alert('ERROR')
+    if (fano.value.length == 0 || fano.value >= ano) {
+        window.alert('ERRO')
     } else {
-        var fsex = document.getElementsByName('sex')
-        var genero = ''
         var idade = ano - Number(fano.value)
+        genero = ''
+        var fsex = document.getElementsByName('sex')
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
-        
+
         if (fsex[0].checked) {
             genero = 'Homem'
             if (idade >= 5 && idade <= 10) {
@@ -25,21 +25,22 @@ function verificar() {
                 img.setAttribute('src', 'imagens/idoso-h.png')
             }
         } else if (fsex[1].checked) {
-                   genero= 'Mulher'
-                   if (idade >= 5 && idade <= 10) {
-                    img.setAttribute('src', 'imagens/bebe-h.png')
-                   } else if (idade <= 25) {
-                    img.setAttribute('src', 'imagens/jovem-h.png')
-                   } else if (idade <= 50) {
-                    img.setAttribute('src', 'imagens/adulto-h.png')
-                   } else {
-                    img.setAttribute('src','imagens/idoso-h.png')
-                   }
-        }
- 
-    res.innerHTML = `Detectamos ${idade} anos e  ${genero}` 
-    res.style.textAlign = 'center'
-    res.appendChild(img)
+            genero = 'Mulher'
+            if (idade >= 5 && idade <= 10) {
+                img.setAttribute('src', 'imagens/bebe-m.png')
+            } else if (idade <= 25) {
+                img.setAttribute('src', 'imagens/jovem-m.png')
+            } else if (idade <= 50) {
+                img.setAttribute('src', 'imagens/adulto-m.png')
+            } else {
+                img.setAttribute('src', 'imagens/idoso-m.png')
+            }
+        } 
+        
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} e ${idade} anos.`
+        res.appendChild(img)
+        
     }
     
 }
