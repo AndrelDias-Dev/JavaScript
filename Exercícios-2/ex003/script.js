602bbe995e91811b00bb72fcdbb5ee7c3dd3ceca
AndrelDias-Dev <andre_guki@hotmail.com>
2026-04-25 16:49:34 -0300

    ex003 adicionado

--- Exercícios-2/ex003/script.js
+++ Exercícios-2/ex003/script.js
@@ -1,30 +1,32 @@
 function gerar() {
-var ini = document.getElementById('ini')
-var fim = document.getElementById('fim')
-var pas = document.getElementById('pas')
-var res = document.querySelector('div#res')
+    ini = document.getElementById('ini')
+    fim = document.getElementById('fim')
+    pas = document.getElementById('pas')
+    res = document.getElementById('res')
 
-if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
-    window.alert('ERROR')
-} else {
-    res.innerHTML = `Contador: `
-    i = Number(ini.value)
-    f = Number(fim.value)
-    p = Number(pas.value)
-
-    if (i < f) {
-        for (var c = i; c <= f; c+= p) {
-        res.innerHTML += ` ${c} \u{1F449} `
-    }
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
+        window.alert('ERRO')
     } else {
-        for (var c = i; c >= f; c-= p) {
-            res.innerHTML += ` ${c} \u{1F449} `
+        var i = Number(ini.value)
+        var f = Number (fim.value)
+        var p = Number (pas.value)
+        res.innerHTML = ''
+    
+
+        if (i < f) {
+            for (var c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1F448}`
         }
+        } else {
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F448}`
+            }
+           
+        }
+
+        res.innerHTML += ` ${c} \u{1F3F3}`
+
+        
+        
     }
-
-    res.innerHTML += ` ${c} \u{1F3F4} `
-
-}
-
-
 }
