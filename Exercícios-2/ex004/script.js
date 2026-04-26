4aba6808b71abc704d5df02d9c6303018b70b5ac
AndrelDias-Dev <andre_guki@hotmail.com>
2026-04-26 02:22:48 -0300

    ex004 adicionado

--- Exercícios-2/ex004/script.js
+++ Exercícios-2/ex004/script.js
@@ -1,19 +1,20 @@
 function gerar() {
     var num = document.getElementById('num')
-    var tab = document.getElementById('tab')
+    var tab = document.getElementById('seletab')
 
     if (num.value.length == 0) {
-        window.alert('erro')
+        window.alert('ERRO')
     } else {
         var n = Number(num.value)
         tab.innerHTML = ''
 
-        for (var c = 1; c <= 10; c++) {
-        var item = document.createElement('option')
-        item.text = `${n} x ${c} = ${n*c}`
-        item.value = `tab${c}`
-        tab.appendChild(item)
+        for (c = 1; c <= 10; c++) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `sele${c}`
+            
 
+            tab.appendChild(item)
         }
     }
 }
