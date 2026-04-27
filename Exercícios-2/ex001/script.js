72df253dbe4bfeaec32eaab4315a2e3f9cac2b85
AndrelDias-Dev <andre_guki@hotmail.com>
2026-04-27 20:42:58 -0300

    ex001 atualizado

--- Exercícios-2/ex001/script.js
+++ Exercícios-2/ex001/script.js
@@ -1,19 +1,19 @@
 function carregar() {
-    var img = document.getElementById('imagem')
+    var img = document.getElementById('img')
     var msg = document.getElementById('msg')
     var data = new Date()
     var hora = data.getHours()
 
-    if (hora >= 5 && hora < 12) {
-        msg.innerHTML = `São ${hora} horas agora, Bom dia!`
-    
-    } else if (hora < 18) {
-       msg.innerHTML = `São ${hora} horas agora, Boa tarde!`
-       img.src = 'imagens/tarde.png'
-    } else if (hora <= 24) {
-        msg.innerHTML = `São ${hora} horas agora, Boa noite`
-        img.src = 'imagens/noite.png'
+    if (hora >= 5 && hora <= 11) {
+        msg.innerHTML = `Bom dia! Agora são ${hora} horas.`
+        document.body.style.background = 'green'
+    } else if (hora <= 18) {
+        msg.innerHTML = `Boa tarde! Agora são ${hora} horas.`
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = 'orange'
     } else {
-        msg.innerHTML = `São ${hora} horas agora, Boa Madrugada`
+        msg.innerHTML = `Boa noite! Agora são ${hora} horas.`
+        img.src = 'imagens/noite.png'
+        document.body.style.background = 'black'
     }
 }
