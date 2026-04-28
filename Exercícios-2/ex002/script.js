f6fc9b8bdfb31e66b3175f83e939fa336ed84d3b
AndrelDias-Dev <andre_guki@hotmail.com>
2026-04-27 21:34:46 -0300

    ex002 atualizado

--- Exercícios-2/ex002/script.js
+++ Exercícios-2/ex002/script.js
@@ -1,17 +1,18 @@
 function verificar() {
+    var num = document.getElementById('ano')
     var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('ano')
-    var res = document.getElementById('res')
+    var ano = data.getFullYear() 
 
-    if (fano.value.length == 0 || fano.value >= ano) {
+    if (num.value.length == 0 || num.value.length >= ano) {
         window.alert('ERRO')
     } else {
-        var idade = ano - Number(fano.value)
-        genero = ''
+        var idade = ano - Number(num.value)
+        var genero = ''
+        var res = document.getElementById('res')
         var fsex = document.getElementsByName('sex')
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
+        img.width = 300
 
         if (fsex[0].checked) {
             genero = 'Homem'
@@ -28,19 +29,16 @@ function verificar() {
             genero = 'Mulher'
             if (idade >= 5 && idade <= 10) {
                 img.setAttribute('src', 'imagens/bebe-m.png')
-            } else if (idade <= 25) {
+            } else if (idade <= 25){
                 img.setAttribute('src', 'imagens/jovem-m.png')
             } else if (idade <= 50) {
-                img.setAttribute('src', 'imagens/adulto-m.png')
+                img.setAttribute('src','imagens/adulto-m.png')
             } else {
                 img.setAttribute('src', 'imagens/idoso-m.png')
             }
-        } 
-        
+        }
+        res.innerHTML = `Detectamos ${idade} anos e ${genero}`
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} e ${idade} anos.`
         res.appendChild(img)
-        
     }
-    
 }
