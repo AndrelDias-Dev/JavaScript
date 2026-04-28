593c4ddd463b9af213fca6f856032dfcd70b68b8
AndrelDias-Dev <andre_guki@hotmail.com>
2026-04-27 23:10:13 -0300

    ex004 atualizado

--- Exercícios-2/ex004/script.js
+++ Exercícios-2/ex004/script.js
@@ -1,18 +1,18 @@
 function gerar() {
     var num = document.getElementById('num')
     var tab = document.getElementById('seletab')
+    var res = document.getElementById('res')
 
     if (num.value.length == 0) {
         window.alert('ERRO')
     } else {
-        var n = Number(num.value)
         tab.innerHTML = ''
 
-        for (c = 1; c <= 10; c++) {
+        for (var c = 1; c <= 10; c++) {
+            var n = Number(num.value)
             var item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
-            item.value = `sele${c}`
-            
+            item.text = `${n} x ${c} = ${c*n}`
+
 
             tab.appendChild(item)
         }
