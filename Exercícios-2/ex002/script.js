b0cb6816fe11304feebaaad567edd594d3619dee
AndrelDias-Dev <andre_guki@hotmail.com>
2026-04-29 16:51:32 -0300

    ex002 adicionado

--- Exercícios-2/ex002/script.js
+++ Exercícios-2/ex002/script.js
@@ -1,20 +1,18 @@
 function verificar() {
-    var num = document.getElementById('ano')
+    var ano = document.getElementById('nas')
     var data = new Date()
-    var ano = data.getFullYear() 
+    var fano = data.getFullYear()
 
-    if (num.value.length == 0 || num.value.length >= ano) {
+    if (ano.value == 0 || ano.value >= fano) {
         window.alert('ERRO')
     } else {
-        var idade = ano - Number(num.value)
-        var genero = ''
-        var res = document.getElementById('res')
         var fsex = document.getElementsByName('sex')
+        var idade = fano - Number(ano.value)
+        var res = document.getElementById('res')
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
-        img.width = 300
 
-        if (fsex[0].checked) {
+        if (fsex[0].checked){
             genero = 'Homem'
             if (idade >= 5 && idade <= 10) {
                 img.setAttribute('src', 'imagens/bebe-h.png')
@@ -27,17 +25,18 @@ function verificar() {
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
-            if (idade >= 5 && idade <= 10) {
-                img.setAttribute('src', 'imagens/bebe-m.png')
-            } else if (idade <= 25){
+          if (idade >= 5 && idade <= 10) {
+            img.setAttribute('src', 'imagens/bebe-m.png')
+            } else if (idade <= 25) {
                 img.setAttribute('src', 'imagens/jovem-m.png')
             } else if (idade <= 50) {
-                img.setAttribute('src','imagens/adulto-m.png')
+                img.setAttribute('src', 'imagens/adulto-m.png')
             } else {
                 img.setAttribute('src', 'imagens/idoso-m.png')
             }
         }
-        res.innerHTML = `Detectamos ${idade} anos e ${genero}`
+
+        res.innerHTML = `Detectamos ${idade} anos e ${genero}.`
         res.style.textAlign = 'center'
         res.appendChild(img)
     }
