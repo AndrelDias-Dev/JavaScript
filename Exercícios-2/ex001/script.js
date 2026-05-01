67a020a2264921877de1840c4a401de06273ad02
AndrelDias-Dev <andre_guki@hotmail.com>
2026-05-01 19:26:47 -0300

    ex001 adicionado

--- Exercícios-2/ex001/script.js
+++ Exercícios-2/ex001/script.js
@@ -1,19 +1,24 @@
-function carregar() {
-    var img = document.getElementById('img')
-    var msg = document.getElementById('msg')
-    var data = new Date()
-    var hora = data.getHours()
+document.addEventListener('DOMContentLoaded', () => {
+    const msg = document.getElementById('msg');
+    const img = document.getElementById('img');
+    const hora = new Date().getHours();
 
-    if (hora >= 5 && hora <= 11) {
-        msg.innerHTML = `Bom dia! Agora são ${hora} horas.`
-        document.body.style.background = 'green'
-    } else if (hora <= 18) {
-        msg.innerHTML = `Boa tarde! Agora são ${hora} horas.`
+    if (hora >= 6 && hora <= 11) {
+        msg.innerHTML = `Bom dia!`
+        img.src = 'imagens/manha.png'
+        document.body.style.background = 'blue'
+
+    } else if (hora >= 12 && hora <= 17) {
+        msg.innerHTML = `Boa tarde!`
         img.src = 'imagens/tarde.png'
         document.body.style.background = 'orange'
-    } else {
-        msg.innerHTML = `Boa noite! Agora são ${hora} horas.`
+    } else if (hora <= 23) {
+        msg.innerHTML = `Boa noite!`
         img.src = 'imagens/noite.png'
         document.body.style.background = 'black'
+    } else {
+        msg.innerHTML = `Boa madrugada!`
     }
-}
+
+
+})
