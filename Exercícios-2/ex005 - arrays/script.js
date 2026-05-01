f3b9fdb1fb54cae89562a3e34e846fee9d460cb8
AndrelDias-Dev <andre_guki@hotmail.com>
2026-05-01 00:42:30 -0300

    ex005 adicionado

--- Exercícios-2/ex005 - arrays/script.js
+++ Exercícios-2/ex005 - arrays/script.js
@@ -1,27 +1,17 @@
 function gerar() {
     var res = document.getElementById('res')
-    let filme = ['Maze Runner, ', 'Os Vingadores, ', 'Resident Evil, ', 'O Cara de Barro, ', 'Uma noite no Museu, ']
 
-    let novos = ['Panico 7, ', 'Michael Jackson, ', 'Zorro.']
-
-    
-    filme.push(...novos)
-    filme.sort()
-
-    
-    
+    let filme = ['Maze Runner, ', 'Os Vingadores, ', 'Resident Evil, ', 'O Cara de Barro, ', 'Uma noite no Museu. ']
 
     res.innerHTML = ''
 
-  /*  for (let pos = 0; pos < filme.length; pos++) {
+    
+  
+
+    
+    for (let pos in filme) {
         res.innerHTML += `${filme[pos]}`
-    } */
-
-        // A duas formas são iguais, mas a de baixo está abreviada.
-
-   for (let pos in filme) {
-        res.innerHTML += ` ${filme[pos]} `
-    }  
+    }
 
     
 }
