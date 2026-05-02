2a9f276057c74e719812ad1fc2587af653a4bb3e
AndrelDias-Dev <andre_guki@hotmail.com>
2026-05-01 21:06:38 -0300

    ex002 adicionado

--- Exercícios-2/ex002/script.js
+++ Exercícios-2/ex002/script.js
@@ -1,19 +1,19 @@
 function verificar() {
-    var ano = document.getElementById('nas')
-    var data = new Date()
-    var fano = data.getFullYear()
+    var res = document.getElementById('res')
+    var fano = document.getElementById('ano')
+    var ano = new Date().getFullYear()
 
-    if (ano.value == 0 || ano.value >= fano) {
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('ERRO')
     } else {
         var fsex = document.getElementsByName('sex')
-        var idade = fano - Number(ano.value)
-        var res = document.getElementById('res')
+        var genero = ''
+        var idade = ano - Number(fano.value)
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
 
-        if (fsex[0].checked){
-            genero = 'Homem'
+        if (fsex[0].checked) {
+            genero = 'Homem' 
             if (idade >= 5 && idade <= 10) {
                 img.setAttribute('src', 'imagens/bebe-h.png')
             } else if (idade <= 25) {
@@ -23,10 +23,10 @@ function verificar() {
             } else {
                 img.setAttribute('src', 'imagens/idoso-h.png')
             }
-        } else if (fsex[1].checked) {
+        } else if (fsex[1].checked) { 
             genero = 'Mulher'
-          if (idade >= 5 && idade <= 10) {
-            img.setAttribute('src', 'imagens/bebe-m.png')
+            if (idade >= 5 && idade <= 10) {
+                img.setAttribute('src', 'imagens/bebe-m.png')
             } else if (idade <= 25) {
                 img.setAttribute('src', 'imagens/jovem-m.png')
             } else if (idade <= 50) {
@@ -36,7 +36,8 @@ function verificar() {
             }
         }
 
-        res.innerHTML = `Detectamos ${idade} anos e ${genero}.`
+
+        res.innerHTML = `Detectamos ${idade} anos e ${genero}`
         res.style.textAlign = 'center'
         res.appendChild(img)
     }
