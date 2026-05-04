ba8f9ec953d904536ad1d87ce216274f4f6a0b38
AndrelDias-Dev <andre_guki@hotmail.com>
2026-05-04 19:33:42 -0300

    ex006 adicionado

--- Exercícios-2/ex006 - functions/script.js
+++ Exercícios-2/ex006 - functions/script.js
@@ -1,3 +1,7 @@
-
+let num = document.getElementById('num')
+let btnAtt = document.getElementById('btnAtt')
+let btnFisnish = document.getElementById('btnFinish')
+let flist = document.getElementById('flist')
+let res = document.getElementById('res')
 
 
