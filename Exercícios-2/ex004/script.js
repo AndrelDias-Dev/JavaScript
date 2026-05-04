79a19f9e59a87d7ca377cd71482d34280407bf10
AndrelDias-Dev <andre_guki@hotmail.com>
2026-05-04 05:52:45 -0300

    ex004 adicionado

--- Exercícios-2/ex004/script.js
+++ Exercícios-2/ex004/script.js
@@ -1,22 +1,22 @@
-const btn = document.getElementById('btn')
-const num = document.getElementById('num')
-
-btn.addEventListener('click', () => {
-    if (num.value == 0) {
-        window.alert('ERROR')
-    } else {
-        const n = num.value
-        const tab = document.getElementById('seletab')
-
-        tab.innerHTML = ''
-
-        for (let c = 1; c <= 10; c++) {
-            const item = document.createElement('option')
-            item.text += `${n} x ${c} = ${n*c}`
+let num = document.getElementById('num')
+let tab = document.getElementById('seletab')
 
 
-             tab.appendChild(item)
-        }
 
+document.getElementById('btn').addEventListener('click', () => {
+    tab.innerHTML = ''
+    
+    for (let c = 1; c < 10; c++) {
+        let item = document.createElement('option')
+        let n = num.value
+
+
+        item.text += `${n} x ${c} = ${n*c}`
+        tab.appendChild(item)
     }
+
+
+
+    
 })
+
