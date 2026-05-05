4bd54681c59cfa11ef102a805fd317cdf2cea741
AndrelDias-Dev <andre_guki@hotmail.com>
2026-05-05 04:28:29 -0300

    ex002 adicionado

--- Exercícios-2/ex001/script.js
+++ Exercícios-2/ex001/script.js
@@ -1,24 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const msg = document.getElementById('msg');
-    const img = document.getElementById('img');
-    const hora = new Date().getHours();
+    let msg = document.getElementById('msg');
+    let img = document.getElementById('img');
+    let hora = new Date().getHours();
 
-    if (hora >= 6 && hora <= 11) {
-        msg.innerHTML = `Bom dia!`
+    if (hora >= 5 && hora <= 11) {
+        msg.innerHTML = 'Bom dia!'
         img.src = 'imagens/manha.png'
-        document.body.style.background = 'blue'
-
-    } else if (hora >= 12 && hora <= 17) {
-        msg.innerHTML = `Boa tarde!`
+        document.body.style.background = 'green'
+    } else if (hora >= 12 && hora <= 18) {
+        msg.innerHTML = 'Boa tarde!'
         img.src = 'imagens/tarde.png'
         document.body.style.background = 'orange'
-    } else if (hora <= 23) {
-        msg.innerHTML = `Boa noite!`
+    } else {
+        msg.innerHTML = 'Boa noite!'
         img.src = 'imagens/noite.png'
         document.body.style.background = 'black'
-    } else {
-        msg.innerHTML = `Boa madrugada!`
     }
-
-
 })
