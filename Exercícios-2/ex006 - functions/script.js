1f59ef425d4fcdcd13b0dbac4910b0a156ebf8be
AndrelDias-Dev <andre_guki@hotmail.com>
2026-05-04 23:58:08 -0300

    ex006 adicionado

--- Exercícios-2/ex006 - functions/script.js
+++ Exercícios-2/ex006 - functions/script.js
@@ -1,7 +1,37 @@
 let num = document.getElementById('num')
-let btnAtt = document.getElementById('btnAtt')
-let btnFisnish = document.getElementById('btnFinish')
-let flist = document.getElementById('flist')
+let lista = document.getElementById('flist')
 let res = document.getElementById('res')
+let compras = []
+
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+
+
+
+document.getElementById('adicionar').addEventListener('click', () => {
+
+    if (isNumero(num.value) && !inLista(num.value, compras)) {
+        window.alert('tudo ok')
+    } else {
+        window.alert('Valor inválido ou já adicionado na lista.')
+    }
+})
+
 
 
