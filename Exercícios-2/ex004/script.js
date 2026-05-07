b2222ed59e6ed1eb66608e2b43f7c6c2c1ceb981
AndrelDias-Dev <andre_guki@hotmail.com>
2026-05-07 03:56:02 -0300

    ex004 adicionado

--- Exercícios-2/ex004/script.js
+++ Exercícios-2/ex004/script.js
@@ -1,23 +1,18 @@
 
-
 document.getElementById('btn').addEventListener('click', () => {
-
-    let num = document.getElementById('num')
-    let tab = document.getElementById('seletab')
-    let n = Number(num.value)
+    const tab = document.getElementById('seletab')
+    const num = document.getElementById('num')
+    const res = document.getElementById('res')
     tab.innerHTML = ''
-
     
 
     for (let c = 1; c <= 10; c++) {
-
         let item = document.createElement('option')
+        let n = Number(num.value)
 
-        item.text += `${n} x ${c} = ${n * c}`
+        item.text += `${n} x ${c} = ${n*c}`
 
         tab.appendChild(item)
     }
-
-
-
+    
 })
