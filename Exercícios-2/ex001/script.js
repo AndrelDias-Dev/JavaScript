66c16e9f4b367015d30df858cb04b41ab69c1d7d
AndrelDias-Dev <andre_guki@hotmail.com>
2026-05-07 00:31:04 -0300

    ex001 adicionado

--- Exercícios-2/ex001/script.js
+++ Exercícios-2/ex001/script.js
@@ -1,19 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let msg = document.getElementById('msg');
-    let img = document.getElementById('img');
-    let hora = new Date().getHours();
+let msg = document.getElementById('msg')
+let img = document.getElementById('img')
+let hora = new Date().getHours()
 
-    if (hora >= 5 && hora <= 11) {
-        msg.innerHTML = 'Bom dia!'
-        img.src = 'imagens/manha.png'
-        document.body.style.background = 'green'
-    } else if (hora >= 12 && hora <= 18) {
-        msg.innerHTML = 'Boa tarde!'
-        img.src = 'imagens/tarde.png'
-        document.body.style.background = 'orange'
-    } else {
-        msg.innerHTML = 'Boa noite!'
-        img.src = 'imagens/noite.png'
-        document.body.style.background = 'black'
-    }
-})
+
+if (hora >= 5 && hora <= 11) {
+    msg.innerHTML = 'Bom dia!'
+    img.src = 'imagens/manha.png'
+} else if (hora >= 12 && hora <= 17) {
+    msg.innerHTML = 'Boa Tarde!'
+    img.src = 'imagens/tarde.png'
+} else if (hora >= 18 && hora <= 23){
+    msg.innerHTML = 'Boa Noite!'
+    img.src = 'imagens/noite.png'
+} else {
+    msg.innerHTML = 'Boa Madruga!'
+}
+
+
+
+
