81c0e9ec957b5f82178cb8de330198b4f9ba5360
AndrelDias-Dev <andre_guki@hotmail.com>
2026-05-13 02:37:57 -0300

    ex003 adicionado

--- Exercícios-2/ex003/script.js
+++ Exercícios-2/ex003/script.js
@@ -1,28 +1,31 @@
+const btn = document.getElementById('btn')
 
-document.getElementById('btn').addEventListener('click', () => {
+
+
+btn.addEventListener('click', () => {
 
     const ini = document.getElementById('ini')
     const fim = document.getElementById('fim')
     const pas = document.getElementById('pas')
     const res = document.getElementById('res')
+
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(pas.value)
+    
     res.innerHTML = ''
 
     if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
-        window.alert('Error')
-    } else {
-        i = Number(ini.value)
-        f = Number(fim.value)
-        p = Number(pas.value)
+    window.alert('Error')
 
-        if (i < f) {
-            for (let c = i; c < f; c += p) {
-             res.innerHTML += `👉 ${c}`
-        }
-        } else {
-            for (let c = i; c > f; c -= p) {
-             res.innerHTML += `👉 ${c}`
-        }
-        }
-            
+} else {
+
+    
+    
+
+    for (let c = i; c < f; c += p) {
+        res.innerHTML += `👉 ${c} `
     }
+    
+}
 })
