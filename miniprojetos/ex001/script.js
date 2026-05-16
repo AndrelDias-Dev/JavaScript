2aa237a3d026cc111fe20e936ecfc8247eabb0e4
AndrelDias-Dev <andre_guki@hotmail.com>
2026-05-16 04:35:26 -0300

    ex001 adicionado

--- miniprojetos/ex001/script.js
+++ miniprojetos/ex001/script.js
@@ -1,42 +1,17 @@
 const btnMenu = document.querySelector('.btn-menu')
 const menu = document.querySelector('.menu')
-const btnLogin = document.querySelector('.btn-login')
-const links = document.querySelectorAll('.menu-link')
-const modal = document.querySelector('#modal')
-const btnClose = document.querySelector('#closeModal')
-const btnConfirm = document.querySelector('#confirmModal')
-
 
 btnMenu.addEventListener('click', () => {
-    menu.classList.toggle('active')
+
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active')
+    } else {
+        menu.classList.add('active')
+    }
+
+
 })
 
-btnClose.addEventListener('click', () => {
-    
-    modal.classList.remove('active')
-
-    modal.close()
-})
-btnLogin.addEventListener('click', () => {
-
-    modal.classList.toggle('active')
-    
-
-    modal.showModal()
-})
-
-
-
-modal.addEventListener('click', (event) => {
-    if (event.target === modal) {
-        
-        modal.classList.remove('active')
-
-        modal.close()
-
-        
-    } 
-})
 
 
 
