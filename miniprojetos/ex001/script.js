1b7712a65261c6886a517fd6253e7f12d4133046
AndrelDias-Dev <andre_guki@hotmail.com>
2026-05-28 17:36:29 -0300

    ex001 adicionado

--- miniprojetos/ex001/script.js
+++ miniprojetos/ex001/script.js
@@ -1,24 +1,27 @@
 const btnMenu = document.querySelector('.btn-menu')
 const menu = document.querySelector('.menu')
 const menuLink = document.querySelectorAll('.menu-link')
-const menuIcon = btnMenu.querySelector('i')
+const iconMenu = btnMenu.querySelector('i')
+
 
 btnMenu.addEventListener('click', () => {
 
     menu.classList.toggle('active')
 
-    if (menu.classList.contains('active')) {
-        menuIcon.classList.remove('fa-bars')
-        menuIcon.classList.add('fa-xmark')
+    if (menu.classList.contains('active') ) {
+        iconMenu.classList.remove('fa-bars')
+        iconMenu.classList.add('fa-xmark')
     } else {
-        menuIcon.classList.remove('fa-xmark')
-        menuIcon.classList.add('fa-bars')
-  }
+        iconMenu.classList.remove('fa-xmark')
+        iconMenu.classList.add('fa-bars')
+    }
 })
 
 menuLink.forEach(link => {
     link.addEventListener('click', () => {
         menu.classList.remove('active')
-        menuIcon.classList.replace('fa-xmark', 'fa-bars')
+
+        iconMenu.classList.remove('fa-xmark')
+        iconMenu.classList.add('fa-bars')
     })
 })
