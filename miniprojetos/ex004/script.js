0cbfae5025d1feda1218396c07d810d7c317c603
AndrelDias-Dev <andre_guki@hotmail.com>
2026-05-31 01:50:04 -0300

    ex004 adicionado

--- miniprojetos/ex004/script.js
+++ miniprojetos/ex004/script.js
@@ -1,6 +1,23 @@
+const btnTab = document.querySelectorAll('.btn-tab')
 
+btnTab.forEach(tab => tab.addEventListener('click', () => tabClicked(tab)));
+
+const tabClicked = (tab) => {
+    btnTab.forEach(tab => tab.classList.remove('active'))
+    tab.classList.add('active')
+
+    const contents = document.querySelectorAll('.content')
+    contents.forEach(content => content.classList.remove('show'))
+
+    const contentId = tab.getAttribute('content-id')
+    const content = document.getElementById(contentId)
+
+
+
+    content.classList.add('show')
 
     
+}
     
     
 
