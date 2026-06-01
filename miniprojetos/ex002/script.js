a59701768a9c2ce983d8d6d0b2dc4c2c9a10b6e4
AndrelDias-Dev <andre_guki@hotmail.com>
2026-06-01 06:17:25 -0300

    ex002 adicionado

--- miniprojetos/ex002/script.js
+++ miniprojetos/ex002/script.js
@@ -1,23 +1,4 @@
 const btnOpen = document.querySelector('.open-modal')
-const btnClose = document.querySelector('.btn-close')
 const modal = document.querySelector('.modal')
-let clickStartedInside = false
 
 btnOpen.addEventListener('click', () => {modal.showModal()})
-btnClose.addEventListener('click', () => {modal.close()})
-
-modal.addEventListener('mousedown', () => {
-    
-    if (event.target !== modal) {
-        clickStartedInside = true
-    } else {
-        clickStartedInside = false
-    }
-})
-
-modal.addEventListener('mouseup', (event) => {
-    
-    if (!clickStartedInside && event.target === modal) {
-        modal.close()
-    }
-})
