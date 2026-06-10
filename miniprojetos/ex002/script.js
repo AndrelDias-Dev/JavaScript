9281f43aedf11838e8c66973cfdbd98b277afaf4
AndrelDias-Dev <andre_guki@hotmail.com>
2026-06-10 20:22:21 -0300

    ex002 adicionado

--- miniprojetos/ex002/script.js
+++ miniprojetos/ex002/script.js
@@ -1,6 +1,5 @@
 const btnOpen = document.querySelector('.open-modal')
-const btnClose = document.querySelector('.btn-close')
 const modal = document.querySelector('.modal')
 
 btnOpen.addEventListener('click', () => {modal.showModal()})
-btnClose.addEventListener('click', () => {modal.close()})
+
