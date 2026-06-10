09370d6ae4781e2c8a57fd0471b567efd55d86b4
AndrelDias-Dev <andre_guki@hotmail.com>
2026-06-10 00:17:58 -0300

    ex005 adicionado

--- miniprojetos/ex005/script.js
+++ miniprojetos/ex005/script.js
@@ -1,5 +1,14 @@
-document.querySelector('.open-btn').addEventListener('click', () => {
+const btnOpen = document.querySelector('.btn-open').addEventListener('click', () => {
     document.querySelector('.sidebar').classList.toggle('open-sidebar')
 
     
 })
+
+const items = document.querySelectorAll('.item');
+
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            items.forEach(i => i.classList.remove('active')); // remove de todos
+            item.classList.add('active'); // adiciona só no clicado
+        });
+    });
