d827332fc3ce7e1cad6e36f12cc410792f9df320
AndrelDias-Dev <andre_guki@hotmail.com>
2026-06-10 23:31:17 -0300

    ex003 adicionado

--- miniprojetos/ex003/script.js
+++ miniprojetos/ex003/script.js
@@ -3,6 +3,7 @@ const accordions = document.querySelectorAll('.accordion')
 accordions.forEach(accordion => {
     accordion.addEventListener('click', () => {
         const body = accordion.querySelector('.accordion-body')
+    
 
         body.classList.toggle('active')
 
