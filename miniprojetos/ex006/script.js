db985f29d44d02efa0c8067e068adc34e14b7275
AndrelDias-Dev <andre_guki@hotmail.com>
2026-06-21 15:27:36 -0300

    ex006 adicionado

--- miniprojetos/ex006/script.js
+++ miniprojetos/ex006/script.js
@@ -1,31 +1,29 @@
-const btnPrev = document.querySelector('.btn-left')
-const btnNext = document.querySelector('.btn-right')
+const btnPrev = document.querySelector('.btn-prev')
+const btnNext = document.querySelector('.btn-next')
 const arraySlides = document.querySelectorAll('.slide')
 const ul = document.querySelector('ul')
 let index = 0;
 
 function changeSlide() {
-    let slideActive = document.querySelector('.slide.active')
-    slideActive.classList.remove('active')
+    let slideActive = document.querySelector('.slide.active').classList.remove('active')
     arraySlides[index].classList.add('active')
 
-    let activeBullet = document.querySelector('li.active')
-    activeBullet.classList.remove('active')
+    let bulletActive = document.querySelector('li.active').classList.remove('active')
     arrayBullets[index].classList.add('active')
-
 
 }
 
 btnNext.addEventListener('click', () => {
-    index = index + 1;
+    index++
 
     if (index < arraySlides.length) {
         changeSlide()
     } else {
         index = 0;
-
         changeSlide()
     }
+
+
 })
 
 btnPrev.addEventListener('click', () => {
@@ -34,21 +32,30 @@ btnPrev.addEventListener('click', () => {
     if (index < 0) {
         index = arraySlides.length - 1;
         changeSlide()
-
     } else {
         changeSlide()
     }
-
-
+    
 })
 
-for (let i = 0; i < arraySlides.length; i++) {
+for (i = 0; i < arraySlides.length; i++) {
 
-    let li = document.createElement('li')
+    const li = document.createElement('li')
+
     ul.appendChild(li)
 }
 
 document.querySelector('li').classList.add('active')
 
-const arrayBullets = document.querySelectorAll('li')
+const  arrayBullets = document.querySelectorAll('li')
+
+arrayBullets.forEach((bullet, i) => {
+    bullet.addEventListener('click', () => {
+        index = i;
+        changeSlide()
+    })
+})
+
+
+
 
