ccfd20ce4d8f61d9de84229f776b4b5feaf6e74d
AndrelDias-Dev <andre_guki@hotmail.com>
2026-07-14 00:40:18 -0300

    ex003 adicionado

--- miniprojetos/ex003/script.js
+++ miniprojetos/ex003/script.js
@@ -1,8 +1,18 @@
 const accordions = document.querySelectorAll('.accordion')
+const accordionActive = document.querySelector('.accordion-body.active')
 
 accordions.forEach(accordion => {
     accordion.addEventListener('click', () => {
-        const body = accordion.querySelector('.accordion-body') 
-        body.classList.toggle('active')
+
+        const body = accordion.querySelector('.accordion-body')
+
+        if (body.classList.contains('active')) {
+            body.classList.remove('active')
+        } else {
+            document.querySelectorAll('.accordion-body.active').forEach(openBody => {
+                openBody.classList.remove('active')
+            })
+            body.classList.add('active')
+        }
     })
 })
