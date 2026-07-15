b2fa9ac0fe394f285a7a42733df58d8af66a4ed8
AndrelDias-Dev <andre_guki@hotmail.com>
2026-07-15 17:26:14 -0300

    ex006 adicionado

--- miniprojetos/ex006/script.js
+++ miniprojetos/ex006/script.js
@@ -1,60 +1,93 @@
-const btnPrev = document.querySelector('.btn-prev')
-const btnNext = document.querySelector('.btn-next')
-const arraySlides = document.querySelectorAll('.slide')
-const ul = document.querySelector('ul')
-let index = 0;
+const arraySlides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slide-left')
+const btnRight = document.querySelector('.slide-right')
+const slideActive = document.querySelector('.slide.active')
+let ul = document.querySelector('.bullets')
+let index = 0
 
-function changeSlide() {
-    let slideActive = document.querySelector('.slide.active').classList.remove('active')
+function changeSLide() {
+    let slideActive = document.querySelector('.slide.active')
+
+    slideActive.classList.remove('active')
+
     arraySlides[index].classList.add('active')
 
-    let bulletActive = document.querySelector('li.active').classList.remove('active')
+    let bulletActive = document.querySelector(' li.active')
+
+    bulletActive.classList.remove('active')
+
     arrayBullets[index].classList.add('active')
 
 }
 
-btnNext.addEventListener('click', () => {
-    index++
+btnRight.addEventListener('click', () => {
 
-    if (index < arraySlides.length) {
-        changeSlide()
+    if (index < arraySlides.length - 1) {
+
+        index++
+        changeSLide()
     } else {
-        index = 0;
-        changeSlide()
-    }
+        index = 0
 
+        changeSLide()
+    }
 
 })
 
-btnPrev.addEventListener('click', () => {
-    index = index - 1;
+btnLeft.addEventListener('click', () => {
 
-    if (index < 0) {
-        index = arraySlides.length - 1;
-        changeSlide()
+    if (index > 0) {
+        index--
+        
+        changeSLide()
     } else {
-        changeSlide()
+        index = arraySlides.length - 1
+
+        changeSLide()
     }
-    
+
 })
 
-for (i = 0; i < arraySlides.length; i++) {
+setInterval(() => {
+    if (index < arraySlides.length - 1) {
 
-    const li = document.createElement('li')
+        index++
+        changeSLide()
+    } else {
+        index = 0
+
+        changeSLide()
+    }
+}, 3000)
+
+
+for (let i = 0; i < arraySlides.length; i++) {
+
+    let li = document.createElement('li')
 
     ul.appendChild(li)
 }
 
-document.querySelector('li').classList.add('active')
+document.querySelector('.bullets li').classList.add('active')
 
-const  arrayBullets = document.querySelectorAll('li')
+
+const arrayBullets = document.querySelectorAll('.bullets li')
 
 arrayBullets.forEach((bullet, i) => {
     bullet.addEventListener('click', () => {
         index = i;
-        changeSlide()
+        changeSLide()
     })
 })
+
+
+
+
+
+
+
+
+
 
 
 
