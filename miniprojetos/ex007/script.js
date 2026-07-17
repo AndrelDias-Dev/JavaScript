2bbe422215537a49fe8810a8b1d9fb232878fef6
AndrelDias-Dev <andre_guki@hotmail.com>
2026-07-17 02:32:28 -0300

    ex007 adicionado

---
+++ miniprojetos/ex007/script.js
@@ -0,0 +1,16 @@
+const produtos = [
+    {id:1, name: 'Camiseta Masculina', price: 29, discount: false, image: 't-shirt 1.jpg', category: 'roupas' },
+
+    {id:2, name: 'Livro - Yol Gösteren', price: 29, discount: 20, image: 'livro 1.jpg', category: 'livros' },
+
+    {id:3, name: 'Infinix Hot 40i', price: 923.99, discount: false, image: 'livro 1.jpg', category: 'eletronicos' },
+
+    {id:4, name: 'Skincare Luxury', price: 40, discount: 20, image: 'skincare 1.jpg', category: 'roupas' },
+
+    {id:5, name: 'Tenis "Nike" Azul', price: 340.34, discount: false, image: 'shoes 1.jpg', category: 'sports' },
+
+    {id:6, name: '', price: 340.34, discount: false, image: 'shoes 1.jpg', category: 'roupas' },
+
+
+
+];
