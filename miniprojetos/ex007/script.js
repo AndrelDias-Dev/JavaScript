262a9f4e24f96f82a9528d5c0e941d5311631aff
AndrelDias-Dev <andre_guki@hotmail.com>
2026-07-23 11:20:03 -0300

    ex007 adicionado

--- miniprojetos/ex007/script.js
+++ miniprojetos/ex007/script.js
@@ -1,7 +1,27 @@
 const buttons = document.querySelectorAll('.dropdown-btn')
+const menus = document.querySelectorAll('.dropdown-menu')
 
 buttons.forEach(btn => {
     btn.addEventListener('click', () => {
         
+        const dropdown = btn.closest('.dropdown')
+        
+        const menu = dropdown.querySelector('.dropdown-menu')
+        
+        menus.forEach(item => {
+            if (item !== menu) {
+                item.classList.remove('active')
+            }
+        })
+
+        menu.classList.toggle('active')
     })
+})
+
+document.addEventListener('click', (e) => {
+    if (!e.target.closest('.dropdown')) {
+        menus.forEach(menu => {
+            menu.classList.remove('active')
+        })
+    }
 })
