db9a2a8a951541031723530a6931524ccb32a51d
AndrelDias-Dev <andre_guki@hotmail.com>
2026-08-28 22:52:21 -0300

    ex013 adicionado

--- miniprojetos/ex012/app.js
+++ miniprojetos/ex012/app.js
@@ -7,35 +7,11 @@ async function Users() {
         const containerUsers = document.querySelector('#users')
 
 
-
-        data.forEach(user => {
-            containerUsers.innerHTML += `
-            <div>
-                <h2>${user.name}</h2>
-                <p>${user.email}</p>
-            </div>
-            `
-
-
-        })
-
-
-    } catch {
+    }catch {
         console.log('error')
     }
 
 
 }
-
-const produtos = [
-    { id: 101, nome: "X-Bacon" },
-    { id: 102, nome: "Batata" },
-    { id: 103, nome: "X-Salada" },
-    { id: 104, nome: "Refrigerante" }
-];
-
-const posicao = produtos.findIndex(produto => produto.id ===  103)
-
-console.log(posicao)
 
 Users();
---
+++ miniprojetos/ex013/app.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('.users')
+const addBtn = document.querySelector('.add-btn')
+
+const usuarios = [
+    { nome: "João" },
+    { nome: "Maria" },
+    { nome: "Pedro" },
+    { nome: "Marcos" }
+];
+
+addBtn.addEventListener('click', () => {
+    
+})
+
