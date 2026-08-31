5207d7ee16309bdf9c91c6608ae979e6174ded29
AndrelDias-Dev <andre_guki@hotmail.com>
2026-08-31 17:17:03 -0300

    ex013 adicionado

--- miniprojetos/ex013/app.js
+++ miniprojetos/ex013/app.js
@@ -1,22 +1,60 @@
 const container = document.querySelector('.users');
-const nomeInput = document.querySelector('#nome');
 const form = document.querySelector('#form')
+const nomeInput = document.querySelector('#nome')
 
-const usuarios = [];
+const usuarios = [
+    { id: 1, nome: "João" },
+    { id: 2, nome: "Maria" },
+    { id: 3, nome: "Pedro" }
+];
 
 form.addEventListener('submit', (event) => {
+
     event.preventDefault()
 
-    const nome = nomeInput.value;
+    const nome = nomeInput.value.trim()
+
+    if(nome === '') {
+        return;
+    }
 
     const novoUsuario = {
+        id: usuarios.length + 1,
         nome: nome
     }
 
     usuarios.push(novoUsuario)
 
-    console.log(usuarios)
+    
+console.log(usuarios)
+
+    renderizarUsuarios();
+
+    nomeInput.value = ''
+
 })
+
+function renderizarUsuarios() {
+
+    const render = usuarios.map(usuario => {
+        return `
+            <div class="info-render">
+                <div>${usuario.nome}</div>
+                <button class="btn-remove" data-id="${usuario.id}">Excluir</button>
+            </div>
+        `
+    }).join('')
+
+    container.innerHTML = render;
+
+}
+
+
+
+
+
+renderizarUsuarios()
+
 
 
 
